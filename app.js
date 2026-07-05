b0619291d8e703b8954d0843adf1e4adffce9df6
Daw-ibu <dawoodibrahimaero@gmail.com>
2026-07-05 20:16:36 +0530

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyCsLFZCN6MwPiUd6TU6VhvSSPSLh4zzWPg",
     authDomain: "skydaw-8318d.firebaseapp.com",
@@ -67,7 +62,4 @@ async function loadComments() {
       </div>
     `;
   });
-}loadComments();
-
-
-    
+}loadComments(); 
